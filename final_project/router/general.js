06b8609aba36804db099048e318c3599fc40ca2b
AGNutGH <185836291+AGNutGH@users.noreply.github.com>
2026-01-22 08:44:20 -0500

    Add the code for getting the book details based on ISBN using Promise callbacks or async-await with Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,14 +42,17 @@ public_users.get('/', async function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-    const book = books[isbn];
-    
-    if (book) {
-      res.send(JSON.stringify(book, null, 4));
-    } else {
-      return res.status(404).json({message: "Book not found"});
-    }
-  });
+    // Create a promise to handle the search
+    const getBook = new Promise((resolve, reject) => {
+        const book = books[isbn];
+        if (book) resolve(book);
+        else reject("Book not found");
+    });
+
+    getBook
+        .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
+        .catch((err) => res.status(404).json({message: err}));
+});
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
